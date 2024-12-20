4a12bae69b77597bef38950aa7c11740b0830790
Nailton2002 <jnmp25jnmp@gmail.com>
2024-12-20 11:41:30 -0300

    Initial commit

---
+++ src/main/kotlin/com/nailton/techserve/TechserveApplication.kt
@@ -0,0 +1,11 @@
+package com.nailton.techserve
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechserveApplication
+
+fun main(args: Array<String>) {
+	runApplication<TechserveApplication>(*args)
+}
---
+++ src/test/kotlin/com/nailton/techserve/TechserveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nailton.techserve
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechserveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
